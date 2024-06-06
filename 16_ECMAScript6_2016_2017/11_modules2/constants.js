16923f90b1197bd7a94c17d38e36091d71c23be0
Squirrel68 <constantlypeaceinside@gmail.com>
2024-06-06 16:13:07 +0700

    #4 - done JS demo

---
+++ 16_ECMAScript6_2016_2017/11_modules2/constants.js
@@ -0,0 +1,6 @@
+const PI = 3.14159;
+const E = 2.71828;
+const SQRT2 = 1.41421;
+const DB = "mongodb://localhost:27017/ecommerce";
+
+export { PI, E, SQRT2, DB };
---
+++ 16_ECMAScript6_2016_2017/11_modules2/main.js
@@ -0,0 +1,11 @@
+import * as constants from "./constants.js";
+import displayTerminal from "./display.js";
+
+displayTerminal(constants);
+
+// {
+//     PI: 3.14159,
+//     E: 2.71828,
+//     SQRT2: 1.41421,
+//     DB: 'mongodb://localhost:27017/ecommerce'
+//   }
